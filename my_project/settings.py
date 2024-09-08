b3053092d675927821b6ecc5875c9b5863a47d45
Ahmed Mahamed <pw1814mk@students.codeinstitute.net>
2024-09-08 16:47:27 +0000

    Setup heroku commands

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=7eax9q*v1(nt7s$oj_+tgi&wyba!ll#53itl_$!*w2=9pu)32'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-ahmed109eng-djangoproje-dqgowniewex.ws.codeinstitute-ide.net', '.herokuapp.com']
 
